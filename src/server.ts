8340d3d308bda191b2a4dbb948834284704d1f44
Marcelo Nogueira <marcelon27@gmail.com>
2019-06-20 21:37:32 -0300

    ts server

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+
+class Server {
+  public app: express.Application
+
+  constructor() {
+    this.app = express()
+    this.config()
+  }
+
+  config() {
+    this.app.set('port', process.env.PORT || 3000)
+    // Middlewares
+    this.app.use(morgan('dev'))
+    this.app.use(helmet())
+  }
+
+  routes() {
+
+  }
+
+  start() {
+    this.app.listen(this.app.get('port'), () => {
+      console.log('Server on port: ', this.app.get('port'))
+    })
+  }
+}
+
+const server = new Server()
+server.start()
